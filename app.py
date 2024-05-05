658c5a64f5066c9217839cd343b1bc8af72cc2e2
alfa <alfaaistudio@gmail.com.com>
2024-05-05 18:19:12 +0300

    .

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
 @app.route('/sticker', methods=['GET', 'POST'])
 def sticker_endpoint():
     if request.method == 'POST':
